375a2bed1bc7fc86df04c0bd5837cf019875d6c5
akaddour <akaddour@outlook.com>
2020-05-23 14:54:29 +0800

    Update raid-disks.sh

--- raid-disks.sh
+++ raid-disks.sh
@@ -10,7 +10,7 @@ do
 	attached="${attached/$i/}"
 done
 i=0
-RAID_CMD="mdadm --create --verbose --level=mirror /dev/md1 --level 0 --raid-devices "
+RAID_CMD="mdadm --create --verbose --level=mirror /dev/md1 --raid-devices "
 RAID_DISKS=""
 for d in $attached
 do
@@ -21,6 +21,7 @@ do
 done
 RAID_CMD+=$i
 RAID_CMD+=$RAID_DISKS
+RAID_CMD+=" -y"
 sleep 5
 for d in $attached 
 do
