9c15a99df415fe59e886ba9dd8498cd0694dc860
akaddour <akaddour@outlook.com>
2020-05-24 22:01:31 +0800

    Update raid-disks.sh

--- raid-disks.sh
+++ raid-disks.sh
@@ -68,6 +68,7 @@ do
     (echo n; echo p; echo 1; echo ; echo ; echo p; echo t; echo fd; echo p; echo w;) | fdisk ${disk} 
 done
 eval "$RAID_CMD"
+sleep 5
 mkfs.ext4 /dev/md${md}
 sudo mkdir -p /mnt/raid1
 mount -a
